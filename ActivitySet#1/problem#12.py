71435681f32050a901dda594021fa91d947dea11
Rakesh <96762361+Rakesh8864@users.noreply.github.com>
2022-05-23 04:33:06 +0000

    p12 complete

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,26 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+counts = {}
+for line in handle:
+    word = line.split()
+    if len(word) < 3 or word[0] != "From" : continue
+    full_hour = word[5]
+    hour = full_hour.split(":")
+    hour = str(hour[:1])
+    hour = hour[2:4]
+    if hour in counts :
+        counts[hour] = 1 + counts[hour]
+    else :
+        counts.update({hour:1})
+lst = list()
+for k, v in counts.items():
+    new_tup = (k, v)
+    lst.append(new_tup)
+ 
+lst = sorted(lst)    
+for k, v in lst:
+    print(k,v)
