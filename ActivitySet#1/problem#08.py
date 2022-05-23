bd1542ec9cca5a67b258877aff813fc255469d4c
Rakesh <96762361+Rakesh8864@users.noreply.github.com>
2022-05-23 04:01:04 +0000

    po8 complete

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,16 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+ans = 0
+count = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    pod = line
+    pod = pod.find(':')
+    pod = line[pod+1:]
+    pod = pod.strip()
+    pod = float(pod)
+    ans = ans + pod
+    count += 1
+average = ans/count
+print("Average spam confidence:", average)
